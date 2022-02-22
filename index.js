3891ad2ef44688333a89a0c4c49e3ecdf26e4906
Daniel Loureiro Cintra <daniel.cintra@approvafacil.com.br>
2022-02-22 16:31:11 -0300

    novos endpoints

--- index.js
+++ index.js
@@ -11,6 +11,40 @@ app.get("/users", (req, res) => {
   res.send(importData);
 });
 
+app.post("/users", (req, res) => {
+  //     user.add({});
+  console.log(req.body);
+  res.send("Usuário criado com sucesso!");
+
+  res.status = 201;
+});
+
+app.put("/users/:id", (req, res) => {
+  //     const user = users.find(id);
+  //     users.update(data);
+  console.log(req.body);
+  res.status = 202;
+  res.send("Usuário atualizado com sucesso!");
+});
+
+app.delete("/users/:id", (req, res) => {
+  res.status = 200;
+  res.send("Usuário excluído com sucesso!");
+});
+
+app.get("/users/:id", (req, res) => {
+  const users = [
+    { id: "1", nome: "Daniel", telefone: "(11)93390-6518" },
+    { id: "2", nome: "Clovis", telefone: "(11)090999-6518" },
+  ];
+
+  const user = users.find((user) => user.id === req.params.id);
+  const message = "Usuário localizado";
+
+  res.status = 200;
+  res.send({ message, user });
+});
+
 app.listen(port, () => {
   console.log("A api está funcionando na porta" + port);
 });
